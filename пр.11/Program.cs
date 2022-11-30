1f5f32d371b87aeaa2fbc890575738a23720645f
user <user@LAPTOP-VCVPT2J9>
2022-11-30 21:37:14 +0300

    Добавьте файлы проекта.

---
+++ пр.11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, z = 0.001, q = 0, s = 0, x = 0.15;
+            int n = 0;
+            a = x;
+            s = a;
+            while (Math.Abs(a) > z)
+            {
+                q = x * x / (4 * n * n + 1) / (4 * n * n - 1);
+                a *= q;
+                s += a;
+                n++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат");
+            Console.WriteLine();
+            Console.WriteLine("Сумма s=" + Convert.ToString(s));
+            Console.WriteLine("Число членов ряда n=" + Convert.ToString(n));
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу");
+        }
+    }
+}
